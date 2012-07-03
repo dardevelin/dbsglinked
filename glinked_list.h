357fdedb4d3700788a71f18133bb6c483d0e7bf3
Darcy Bras da Silva <dardevelin.cidadecool.com>
2012-07-03 04:26:36 +0100

    fix paramater name for (de)allocator

--- glinked_list.h
+++ glinked_list.h
@@ -40,7 +40,7 @@ typedef struct sGlinkedList {
 } libglinked_list_t;
 //interface
 void libglinked_init_list(libglinked_list_t *, 
-  void *(*allocator)(void*), void(*deallocator)(void*));
+  void *(*node_allocator)(void*), void(*node_deallocator)(void*));
 
 libglinked_node_t *libglinked_create_node(libglinked_list_t *,
   void *, void(*dalloc)(void*));
